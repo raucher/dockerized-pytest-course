7f6d0e2450e356c077409dce6ad0bfbc7754d2ea
maksims <maksims.muizininks@ogf.de>
2024-11-21 18:56:02 +0100

    update pytest.ini

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from scripts import data_processor
 
 @pytest.fixture(scope="module")
 def city_list_location():
-    return 'tests/resources/cities/'
+    return "tests/resources/cities/"
 
 
 @pytest.fixture(scope="module")
@@ -16,7 +16,7 @@ def process_data(city_list_location):
     def _specify_type(file_name_or_type):
         for f in files:
             if file_name_or_type in f:
-                if file_name_or_type != '.json':
+                if file_name_or_type != ".json":
                     data = data_processor.csv_reader(city_list_location + f)
                 else:
                     data = data_processor.json_reader(city_list_location + f)
